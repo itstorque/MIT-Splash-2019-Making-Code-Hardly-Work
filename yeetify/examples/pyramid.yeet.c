22dd713932ae289ec7faa0da7fb69c25d8236f74
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-10-12 13:01:58 -0400

    Added another example program

---
+++ yeetify/examples/pyramid.yeet.c
@@ -0,0 +1,56 @@
+#define YyyyEEt 0
+#define YeEEeEEEeEEeTtTTtt "\n"
+#define yeeEt ++k
+#define yeeEEett "* "
+#define YeEEEEEET 2*i-1
+#define YEetTt !=
+#define yEEeeEeeeEeeeeET k
+#define yYyEEtTt while
+#define YEettt "  "
+#define YYyyEEeET ++space
+#define YyEETtt space<=rows-i
+#define YYYyeEeeEeEET space=1
+#define yEeEEETTT ++i,
+#define YeetttT i<=rows
+#define yeeEeEEEeEttTt i=1
+#define yeEt for
+#define yyyyeEEeeET &rows
+#define yYEeeTtTttTtt scanf
+#define YEeEeEETTTttTt "%d"
+#define yeEttt "Enter number of rows: "
+#define YeeEeEeEttTtT k=0
+#define YEeEEeT rows,
+#define yEeeEeeT space,
+#define YEETTtt i,
+#define yoted ;
+#define yeeeT }
+#define Yeeet {
+#define yeet printf
+#define yeett int
+#define yeeeeT )
+#define Yeeeet (
+#define YEET main
+#define Yeet return
+#include <stdio.h>
+
+yeett YEET Yeeeet yeeeeT
+ Yeeet
+  yeett YEETTtt yEeeEeeT YEeEEeT YeeEeEeEttTtT yoted
+  yeet Yeeeet yeEttt yeeeeT yoted
+  yYEeeTtTttTtt Yeeeet YEeEeEETTTttTt, yyyyeEEeeET yeeeeT yoted
+  yeEt Yeeeet yeeEeEEEeEttTt yoted YeetttT yoted yEeEEETTT YeeEeEeEttTtT yeeeeT
+   Yeeet
+    yeEt Yeeeet YYYyeEeeEeEET yoted YyEETtt yoted YYyyEEeET yeeeeT
+     Yeeet
+      yeet Yeeeet YEettt yeeeeT yoted
+     yeeeT
+    yYyEEtTt Yeeeet yEEeeEeeeEeeeeET YEetTt YeEEEEEET yeeeeT
+     Yeeet
+      yeet Yeeeet yeeEEett yeeeeT yoted
+      yeeEt yoted
+     yeeeT
+    yeet Yeeeet YeEEeEEEeEEeTtTTtt yeeeeT yoted
+   yeeeT
+
+  Yeet YyyyEEt yoted
+ yeeeT
